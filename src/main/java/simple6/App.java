3ef3c26ba0bc981db51248cc4240ac468d2b5787
udayshree123 <udayshree123@gmail.com>
2020-10-01 19:04:12 +0530

    1

---
+++ src/main/java/simple6/App.java
@@ -0,0 +1,10 @@
+package simple6;
+public class App 
+{
+   public static void main(String[] args) {
+    boolean isJavaFun = true;
+    boolean isFishTasty = false;    
+    System.out.println(isJavaFun);
+    System.out.println(isFishTasty);
+  }
+}
